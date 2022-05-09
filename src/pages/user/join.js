0fc4db9d93812d605fe78e2f29ec3f30599ed6d7
N3theri9N <nnea5215@gmail.com>
2022-05-09 21:20:43 +0900

    Update join.js

--- src/pages/user/join.js
+++ src/pages/user/join.js
@@ -1,11 +1,8 @@
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import JoinForm from 'components/user/JoinForm';
-import { loginCheck } from 'utils/loginCheck';
 
 function Landing() {
-
-  loginCheck()
 
   return (
     <div>
