35d511a7f2d34e980a36a86f2ac75c02e919cb28
Juyoung <godante27@gmail.com>
2022-05-11 01:45:52 +0900

    feat: Add Schedule page

--- src/pages/schedules.js
+++ src/pages/schedules.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchSchedules } from 'store/actions/schedule';
+import moment from 'moment';
+import styles from 'styles/schedules.module.css';
+import Schedule from 'components/schedule/Schedule';
+
+const dateFormat = 'YYYY년 MM월 DD일';
 
 function Schedules() {
+  const [date, setDate] = useState(moment().format(dateFormat));
   const [loginUser, setLoginUser] = useState(
     typeof window !== 'undefined'
       ? JSON.parse(window.localStorage.getItem('user'))
       : null,
   );
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,7 +26,32 @@ function Schedules() {
     ),
   );
 
-  return <div>Hello</div>;
+  console.log(schedules);
+
+  const handleDateChange = (type) => {
+    const tmp = moment(date, dateFormat);
+    if (type === 'prev') {
+      setDate(tmp.subtract(1, 'days')).format(dateFormat);
+    }
+    if (type === 'next') {
+      setDate(tmp.add(1, 'days').format(dateFormat));
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>My Schedules</h2>
+      <div className={styles.dateSelector}>
+        <button onClick={() => handleDateChange('prev')}>prev</button>
+        <h3>{date}</h3>
+        <button onClick={() => handleDateChange('next')}>next</button>
+      </div>
+      {schedules.map((schedule, idx) => (
+        <Schedule key={idx} data={schedule} />
+      ))}
+      <button>일정 추가</button>
+    </div>
+  );
 }
 
 export default Schedules;
