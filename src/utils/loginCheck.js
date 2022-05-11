49a44de2a88b9fce2175466b1598fa089cc14ef5
N3theri9N <nnea5215@gmail.com>
2022-05-11 20:15:42 +0900

    loginCheck 수정
    tokenExpire 가 없는 경우 수정

--- src/utils/loginCheck.js
+++ src/utils/loginCheck.js
@@ -2,7 +2,7 @@ const loginCheck = () => {
   if (typeof window !== 'undefined') {
     const userData = JSON.parse(localStorage.getItem('user'));
 
-    if(userData && Number(new Date)/1000 > userData.tokenExpire){
+    if(userData && ( !userData.tokenExpire || Number(new Date)/1000 > userData.tokenExpire)){
       localStorage.removeItem('user');
     }
 
