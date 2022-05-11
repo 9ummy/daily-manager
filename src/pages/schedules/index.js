4d82a3b32873300c7d9900dcb96f5f6676fa5022
Juyoung <godante27@gmail.com>
2022-05-12 00:05:20 +0900

    feat: Add delete schedule function

--- src/pages/schedules/index.js
+++ src/pages/schedules/index.js
@@ -1,32 +1,27 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchSchedules } from 'store/actions/schedule';
 import moment from 'moment';
 import styles from 'styles/schedules.module.css';
 import Schedule from 'components/schedule/Schedule';
 import { deleteSchedule } from 'store/actions/schedule';
+import { loginCheck } from 'utils/loginCheck';
 
 const dateFormat = 'YYYY년 MM월 DD일';
 
 function Schedules() {
-  const [date, setDate] = useState(moment().format(dateFormat));
-  const [loginUser, setLoginUser] = useState(
-    typeof window !== 'undefined'
-      ? JSON.parse(window.localStorage.getItem('user'))
-      : null,
-  );
-
+  const loginUser = loginCheck();
   const dispatch = useDispatch();
+  const router = useRouter();
+
+  const [date, setDate] = useState(moment().format(dateFormat));
+
+  const scheduleState = useSelector((state) => state.schedule);
 
   useEffect(() => {
     dispatch(fetchSchedules());
-  }, [loginUser, dispatch]);
-
-  const schedules = useSelector((state) =>
-    state.schedule.scheduleList.filter(
-      (schedule) => schedule.authorId == loginUser.id,
-    ),
-  );
+  }, [dispatch]);
 
   const handleDateChange = (type) => {
     const tmp = moment(date, dateFormat);
@@ -46,14 +41,19 @@ function Schedules() {
         <h3>{date}</h3>
         <button onClick={() => handleDateChange('next')}>next</button>
       </div>
-      {schedules.map((schedule, idx) => (
-        <Schedule
-          key={idx}
-          data={schedule}
-          handleDeleteSchedule={() => dispatch(deleteSchedule(schedule._id))}
-        />
-      ))}
-      <button>일정 추가</button>
+      {scheduleState.scheduleList
+        .filter((schedule) => schedule.authorId === loginUser.id)
+        .map((schedule, idx) => (
+          <Schedule
+            key={idx}
+            data={schedule}
+            handleDeleteSchedule={() => {
+              dispatch(deleteSchedule(schedule._id));
+              dispatch(fetchSchedules());
+            }}
+          />
+        ))}
+      <button onClick={() => router.push('/schedules/new')}>일정 추가</button>
     </div>
   );
 }
