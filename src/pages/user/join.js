03eff3990540079ee5ab9541163f75f5284c45bc
Juyoung <godante27@gmail.com>
2022-05-13 18:27:24 +0900

    refactor: Replace img tag with next Image

--- src/pages/user/join.js
+++ src/pages/user/join.js
@@ -1,13 +1,18 @@
+import Image from 'next/image';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import JoinForm from 'components/user/JoinForm';
 
 function Landing() {
-
   return (
     <div className="container w-25">
       <div className="row text-center m-5">
-        <img src="/logo.png" />
+        <Image
+          src="/logo.png"
+          alt="daily manager logo"
+          width="372"
+          height="154"
+        />
       </div>
       <JoinForm />
     </div>
