48120316233ce5232dfd6a140cf0a995d06b6cb1
Juyoung <godante27@gmail.com>
2022-05-15 14:59:59 +0900

    style: Add style to Video component

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,12 +1,13 @@
-import {useEffect, useRef, useState} from 'react';
-import axios from "axios";
-import {useSelector} from "react-redux";
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import styles from 'styles/video.module.css';
 
-function Video({
-  width = "50%",
-}) {
+function Video() {
   const [isMuted, setIsMuted] = useState(true);
-  const [src, setSrc] = useState("https://ai-platform-prd.s3.ap-northeast-2.amazonaws.com/projectAssets/3d/capture_1920x1080_2022-05-14_10-19-45_0RP9H.mp4");
+  const [src, setSrc] = useState(
+    'https://ai-platform-prd.s3.ap-northeast-2.amazonaws.com/projectAssets/3d/capture_1920x1080_2022-05-14_10-19-45_0RP9H.mp4',
+  );
 
   const video = useRef();
 
@@ -26,9 +27,10 @@ function Video({
       });
       if (scheduleInTime.length > 0) {
         const videoKey = scheduleInTime[0].videoKey;
-        const videoResponse = axios.post(`/api/video/${videoKey}`, {
-          "token" : JSON.parse(localStorage.getItem('user')).token
-        })
+        const videoResponse = axios
+          .post(`/api/video/${videoKey}`, {
+            token: JSON.parse(localStorage.getItem('user')).token,
+          })
           .then((result) => {
             console.log(result.data.videoSrc);
             setSrc(result.data.videoSrc);
@@ -39,16 +41,19 @@ function Video({
   }, [scheduleState]);
 
   return (
-    <div className="fixed-bottom">
-      <video
-        ref={video}
-        src = {src}
-        autoPlay
-        muted
-        style={{ width: `${width}` }}
-      >
-      </video>
-      <button onClick={unmuteVideo}>{isMuted ? 'Unmute' : 'Mute'}</button>
+    <div className={styles.container}>
+      <div className={styles.videoContainer}>
+        <button className={styles.videoButton} onClick={unmuteVideo}>
+          {isMuted ? 'Unmute' : 'Mute'}
+        </button>
+        <video
+          className={styles.video}
+          ref={video}
+          src={src}
+          autoPlay
+          muted
+        ></video>
+      </div>
     </div>
   );
 }
